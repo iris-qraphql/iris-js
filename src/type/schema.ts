a1719908b432466b4eabae66270b5c4071d94aeb
davit nalchevanidze <daviti.nalchevanidze@accenture.com>
2022-02-04 14:20:51 +0100

    update schema

--- src/type/schema.ts
+++ src/type/schema.ts
@@ -10,6 +10,8 @@ import { getNamedType, isDataType, isResolverType } from './definition';
 import type { GraphQLDirective } from './directives';
 import { isDirective, specifiedDirectives } from './directives';
 
+const notNill = <T>(x: Maybe<T>): x is T => Boolean(x);
+
 /**
  * Test if the given value is a GraphQL schema.
  */
@@ -38,9 +40,8 @@ export class IrisSchema {
 
   constructor(config: Readonly<GraphQLSchemaConfig>) {
     this.__validationErrors = config.assumeValid === true ? [] : undefined;
-
+    this._typeMap = {};
     this.description = config.description;
-
     this._queryType = config.query;
     this._mutationType = config.mutation;
     this._subscriptionType = config.subscription;
@@ -49,31 +50,23 @@ export class IrisSchema {
       [...(config.directives ?? []), ...specifiedDirectives],
     );
 
+    const types: Array<IrisNamedType> = [
+      this._queryType,
+      this._mutationType,
+      this._subscriptionType,
+      ...(config.types ?? []),
+    ].filter(notNill);
+
     // To preserve order of user-provided types, we add first to add them to
     // the set of "collected" types, so `collectReferencedTypes` ignore them.
     const allReferencedTypes: Set<IrisNamedType> = new Set(config.types);
 
-    config.types?.forEach((type) => {
+    types.forEach((type) => {
       allReferencedTypes.delete(type);
       collectReferencedTypes(type, allReferencedTypes);
     });
 
-    [this._queryType, this._mutationType, this._subscriptionType].forEach(
-      (type) =>
-        type ? collectReferencedTypes(type, allReferencedTypes) : undefined,
-    );
-
-    for (const directive of this._directives) {
-      // Directives are not validated until validateSchema() is called.
-      if (isDirective(directive)) {
-        for (const arg of directive.args) {
-          collectReferencedTypes(arg.type, allReferencedTypes);
-        }
-      }
-    }
-
-    // Storing the resulting map for reference by the schema.
-    this._typeMap = Object.create(null);
+    collectDirectiveRefs(this._directives, allReferencedTypes);
 
     for (const namedType of allReferencedTypes) {
       if (namedType == null) {
@@ -100,31 +93,24 @@ export class IrisSchema {
     return 'IrisSchema';
   }
 
-  getQueryType(): IrisResolverType | undefined {
-    return this._queryType ?? undefined;
-  }
+  getQueryType = (): IrisResolverType | undefined =>
+    this._queryType ?? undefined;
 
-  getMutationType(): IrisResolverType | undefined {
-    return this._mutationType ?? undefined;
-  }
+  getMutationType = (): IrisResolverType | undefined =>
+    this._mutationType ?? undefined;
 
-  getSubscriptionType(): IrisResolverType | undefined {
-    return this._subscriptionType ?? undefined;
-  }
+  getSubscriptionType = (): IrisResolverType | undefined =>
+    this._subscriptionType ?? undefined;
 
   getTypeMap = (): TypeMap => this._typeMap;
 
-  getType(name: string): IrisNamedType | undefined {
-    return this.getTypeMap()[name];
-  }
+  getType = (name: string): IrisNamedType | undefined =>
+    this.getTypeMap()[name];
 
-  getDirectives(): ReadonlyArray<GraphQLDirective> {
-    return this._directives;
-  }
+  getDirectives = (): ReadonlyArray<GraphQLDirective> => this._directives;
 
-  getDirective(name: string): Maybe<GraphQLDirective> {
-    return this.getDirectives().find((directive) => directive.name === name);
-  }
+  getDirective = (name: string): Maybe<GraphQLDirective> =>
+    this.getDirectives().find((directive) => directive.name === name);
 }
 
 type TypeMap = ObjMap<IrisNamedType>;
@@ -143,38 +129,56 @@ export interface GraphQLSchemaConfig extends IrisSchemaValidationOptions {
   directives?: Maybe<ReadonlyArray<GraphQLDirective>>;
 }
 
+const collectDirectiveRefs = (
+  directives: ReadonlyArray<GraphQLDirective>,
+  allReferencedTypes: Set<IrisNamedType>,
+) => {
+  for (const directive of directives) {
+    // Directives are not validated until validateSchema() is called.
+    if (isDirective(directive)) {
+      directive.args.forEach((arg) =>
+        collectReferencedTypes(arg.type, allReferencedTypes),
+      );
+    }
+  }
+};
+
 function collectReferencedTypes(
   type: IrisType,
   typeSet: Set<IrisNamedType>,
-): Set<IrisNamedType> {
+): void {
   const namedType = getNamedType(type);
 
-  if (!typeSet.has(namedType)) {
-    typeSet.add(namedType);
-    if (isResolverType(namedType)) {
-      const variants = namedType.variants();
-      if (namedType.isVariantType()) {
-        const fields = Object.values(variants[0]?.fields ?? {});
-        for (const field of fields) {
-          collectReferencedTypes(field.type, typeSet);
-          for (const arg of field.args) {
-            collectReferencedTypes(arg.type, typeSet);
-          }
-        }
-      } else {
-        for (const variant of variants) {
-          if (variant.type) {
-            collectReferencedTypes(variant.type, typeSet);
-          }
-        }
-      }
-    } else if (isDataType(namedType)) {
-      namedType
-        .variants()
-        .flatMap((x) => Object.values(x.fields ?? {}))
-        .forEach((field) => collectReferencedTypes(field.type, typeSet));
-    }
+  if (typeSet.has(namedType)) {
+    return;
   }
 
-  return typeSet;
+  typeSet.add(namedType);
+
+  if (isResolverType(namedType)) {
+    const variants = namedType.variants();
+    if (namedType.isVariantType()) {
+      const fields = Object.values(variants[0]?.fields ?? {});
+      for (const field of fields) {
+        collectReferencedTypes(field.type, typeSet);
+        for (const arg of field.args) {
+          collectReferencedTypes(arg.type, typeSet);
+        }
+      }
+    } else {
+      for (const variant of variants) {
+        if (variant.type) {
+          collectReferencedTypes(variant.type, typeSet);
+        }
+      }
+    }
+    return;
+  }
+
+  if (isDataType(namedType)) {
+    namedType
+      .variants()
+      .flatMap((x) => Object.values(x.fields ?? {}))
+      .forEach((field) => collectReferencedTypes(field.type, typeSet));
+  }
 }
