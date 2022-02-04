d195d059e13950d6c6786142df3ed805c5b8d2f2
davit nalchevanidze <daviti.nalchevanidze@accenture.com>
2022-02-04 09:42:54 +0100

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,16 @@
  */
 
 // The Iris.js version info.
+import type { GraphQLArgs } from 'graphql';
+import { graphql } from 'graphql';
+import type { ExecutionResult } from 'graphql/execution';
+
+import type { IrisSchema } from './type/schema';
+import { validateSchema } from './type/validate';
+
+import { toGQLSchema } from './transpile/toGQLSchema';
+
 export { version, versionInfo } from './version';
-export { iris } from './iris';
 
 export {
   IrisSchema,
@@ -101,3 +109,20 @@ export type {
   ResolverTypeDefinitionNode,
   DirectiveDefinitionNode,
 } from './language/index';
+
+export type IrisArgs = Omit<GraphQLArgs, 'schema'> & {
+  schema: IrisSchema;
+};
+
+export const iris = ({
+  schema,
+  ...args
+}: IrisArgs): Promise<ExecutionResult> => {
+  const errors = validateSchema(schema);
+
+  if (errors.length > 0) {
+    return Promise.resolve({ errors });
+  }
+
+  return graphql({ schema: toGQLSchema(schema), ...args });
+};
