91ad612bcffd07df3b2cf259d3c4714abfe4e097
David Nalchevanidze <d.nalchevanidze@gmail.com>
2022-08-05 14:25:49 +0200

    types

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -6,55 +6,30 @@ import { validateSDL } from '../validation/validate';
 import { parse } from '../parsing';
 import { keyMap } from '../utils/ObjMap';
 
-import type {
-  DirectiveDefinitionNode,
-  DocumentNode,
-  TypeDefinitionNode,
-} from './ast';
-import { isDirectiveDefinitionNode, isTypeDefinitionNode } from './ast';
+import type { DocumentNode, TypeDefinitionNode } from './ast';
+import { isTypeDefinitionNode } from './ast';
 
 type IrisSchema = {
   readonly types: ObjMap<TypeDefinitionNode>;
-  readonly directives: ObjMap<DirectiveDefinitionNode>;
   readonly document: DocumentNode;
 };
 
-type ParseOptions = {
-  noLocation?: boolean;
-};
-
-export const buildSchema = (
-  source: string,
-  options?: ParseOptions,
-): IrisSchema => {
-  const document = parse(source, { noLocation: options?.noLocation });
+export const buildSchema = (source: string,): IrisSchema => {
+  const document = parse(source);
   const errors = validateSDL(document);
 
   if (errors.length !== 0) {
     throw new Error(errors.map((error) => error.message).join('\n\n'));
   }
 
-  const types = keyMap(
-    document.definitions.filter(isTypeDefinitionNode),
-    (x) => x.name.value,
-  );
-
-  const directives = keyMap(
-    document.definitions.filter(isDirectiveDefinitionNode),
-    (x) => x.name.value,
-  );
-
   return {
     document,
-    directives,
-    types,
+    types: keyMap(
+      document.definitions.filter(isTypeDefinitionNode),
+      ({ name }) => name.value,
+    ),
   };
 };
-
-export const getDirective = (
-  schema: IrisSchema,
-  name: string,
-): Maybe<DirectiveDefinitionNode> => schema.directives[name];
 
 export const getType = (
   schema: IrisSchema,
